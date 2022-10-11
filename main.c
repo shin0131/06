abd97c0743bc76424a5d2a0c60fdbc48cd4a52f4
shin0131 <shinyoung@sookmyung.ac.kr>
2022-10-11 17:46:04 +0900

    05

--- main.c
+++ main.c
@@ -3,15 +3,50 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+int get_integer_n(void);
+int get_integer_r(void);
+int factorial(int n);
+int combination(int n, int r);
+
+int main(void)
 {
-	return (a*a);
+	int a, b;
+	a = get_integer_n();
+	b = get_integer_r();
+	
+	printf("C(%d, %d) = %d\n", a, b, combination(a,b));
+	return 0;	
 }
 
-int main()
+int get_integer_n(void)
 {
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n", a);
+	int n;
+	printf("Combination 계산을 위한 n을 입력하시오. : ");
+	scanf("%i", &n);
+	return n;
 }
+
+int get_integer_r(void)
+{
+	int r;
+	printf("Combination 계산을 위한 r을 입력하시오. : ");
+	scanf("%i", &r);
+	return r; 
+}
+
+int factorial(int n)
+{
+	int i;
+	int res = 1;
+	for(i = 1; i <= n; i++)
+	    res = res*i;
+	return res;
+}
+
+int combination(int n, int r)
+{
+	return (factorial(n)/(factorial(r)*factorial(n-r)));
+}
+
+
 
